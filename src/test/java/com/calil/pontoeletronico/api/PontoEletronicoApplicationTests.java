66cecb9c6c362ec97792fc8bc2f8a5003ce198c0
Calil Robson de Mello <calilmello@brq.com>
2019-11-05 11:44:32 -0300

    Commit dos arquivos iniciais

---
+++ src/test/java/com/calil/pontoeletronico/api/PontoEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calil.pontoeletronico.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
